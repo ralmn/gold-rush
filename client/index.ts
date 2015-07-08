55a67302d7fd0cbf445720029cc341afbf03d4b3
Elisée Maurer <elisee@sparklinlabs.com>
2015-07-09 00:01:13 +0200

    Block out the UI

--- client/index.ts
+++ client/index.ts
@@ -3,5 +3,4 @@ import * as io from "socket.io-client";
 let socket = io.connect(null, { transports: [ "websocket" ], reconnect: false });
 
 socket.on("connect", () => {
-  alert("connected!");
 });
